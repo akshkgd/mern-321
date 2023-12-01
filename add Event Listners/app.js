b4a4e83c640877b400bd2da87ba4dd9c11c4a843
Ashish Shukla <akshkgd@gmail.com>
2023-12-01 19:11:20 +0530

    wd

---
+++ add Event Listners/app.js
@@ -0,0 +1,50 @@
+// function test(){
+//     console.log('test function is called!')
+// }
+
+
+// adding the event listner??
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', (e)=>{
+//     console.log(e.target.innerText)
+// })
+
+// target id
+// class
+
+
+// let btn = document.querySelector('#button');
+// btn.addEventListener('click', ()=>{
+//     console.log('button is clicked')
+// })
+
+
+// let title = document.querySelector('h1');
+// title.addEventListener('click', ()=>{
+//     console.log('heading is being clicked!')
+// })
+
+
+// let btns = document.querySelectorAll('.btn')
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click', (e)=>{
+//         console.log(e.target.innerText, ' is Clicked')
+//     })
+// })
+
+
+// let foodItems = document.querySelectorAll('.card');
+// foodItems.forEach((item)=>{
+//     item.addEventListener('click', (e)=>{
+//         console.log(item.dataset.title, item.dataset.price)
+//     })
+// })
+
+
+
+
+// let btn = document.querySelectorAll('.className')
+// btn.addEventListner('click', ()=>{
+
+// })
+
